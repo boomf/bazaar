5772293d048e71db585a9561f55aa4cd5e588a4d
Andrew Hilton <andrewh@mintdigital.com>
2015-03-11 13:16:33 +0000

    updating words

--- lib/bazaar.rb
+++ lib/bazaar.rb
@@ -20,7 +20,7 @@ module Bazaar
     (get_item("superadj") + ' ' + get_item("superitems")).humanize
   end
   def self.heroku
-    get_item("superadj") + '-' + get_item("superitems") + '-' + rand(0-9999).to_s
+    get_item("superadj") + '-' + get_item("superitems") + rand(0-9999).to_s
   end
   def self.get_item(filename)
     File.read(File.expand_path("../bazaar/#{filename}.txt", __FILE__)).split("\n").sample
